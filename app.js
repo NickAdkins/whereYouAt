a9cbd707c02a0f134e936947237ad484422110e0
Bredyn McCombs <d00238943@dmail.dixie.edu>
2016-11-11 10:10:32 -0700

    First commit of server. The file path should be there as well as some minor additions to the routes of the app. Also, the package.json should have everything we need.

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var express = require("express");
+var morgan = requre("morgan");
+var mongoose = require("mongoose");
+var skipper = require ("skipper");
+var colors = require ("colors");
+
+
