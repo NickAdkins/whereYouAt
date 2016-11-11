b31fcf693f24134e8422cc8a0ab562a3c3bb76b5
Bredyn McCombs <d00238943@dmail.dixie.edu>
2016-11-11 11:52:40 -0700

    filled out user/routes.js with all the routes needed for our app... for now.

--- server/users/routes.js
+++ server/users/routes.js
@@ -1,4 +1,16 @@
 var express = require('express');
+var controller = require('"./controller"');
 var router = express.Router()
+
+router.get("/users/:user", controller.retrieve);
+router.get("/users", controller.index);
+// router.get("/users", controller.indexEvent)
+
+router.post("/users", controller.create);
+
+router.put("/users/:user", controller.update);
+
+router.delete("/users/:user", controller.deleteOne);
+router.delete("/users", controller.deleteAll);
 
 module.exports = router
